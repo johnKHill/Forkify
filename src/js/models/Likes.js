88898d6e4297be361f24bab2abd7a1e2548a37ec
Johknotter Hill <johknotterhill@gmail.com>
2019-02-06 20:14:46 -0500

    hopefully a recovery from losing my repository

---
+++ src/js/models/Likes.js
@@ -0,0 +1,42 @@
+export default class Likes {
+    constructor() {
+        this.likes = [];
+    }
+
+    addLike(id, title, authur, img) {
+        const like = {id, title, authur, img};
+        this.likes.push(like);
+
+        // Persist the data in lacalStorage
+        this.persistData();
+
+        return like;
+    }
+
+    deleteLike(id) {
+        const index = this.likes.findIndex(el => el.id === id);
+        this.likes.splice(index, 1);
+
+        // Persist the data in lacalStorage
+        this.persistData();
+    }
+
+    isLiked(id) {
+        return this.likes.findIndex(el => el.id === id) !== -1;
+    }
+
+    getNumLikes() {
+        return this.likes.length;
+    }
+
+    persistData() {
+        localStorage.setItem('likes', JSON.stringify(this.likes));
+    }
+
+    readStorage() {
+        const storage = JSON.parse(localStorage.getItem('likes'));
+
+        // Restore likes from localStorage
+        if (storage) this.likes = storage;
+    }
+}
